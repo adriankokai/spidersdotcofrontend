1f7346df7e67fc3723e8eb79aa5f0f00ba51e50f
Adrian Kokai <adriankokai@gmail.com>
2021-01-31 15:35:22 +0200

    fixed image sizes in LinksInBio

--- src/components/LinkInBio.js
+++ src/components/LinkInBio.js
@@ -5,16 +5,19 @@ export class LinkInBio extends Component {
     render() {
         return (
             <React.Fragment>
-                <a href={this.props.link} className="col s12 m6 l4">
-                    <img style={styles} src={BACKEND_URL + 'main' + this.props.image} alt={this.props.title} />
-                </a>
+                <div className="col s12 m6 l4">
+                    <a href={this.props.link}>
+                        <img style={styles} src={BACKEND_URL + 'main' + this.props.image} alt={this.props.title} />
+                    </a>
+                </div>
             </React.Fragment>
         )
     }
 }
 
 const styles = {
-   marginBottom: '2px'
+   marginBottom: '2px',
+   width: '100%'
 }
 
 export default LinkInBio
