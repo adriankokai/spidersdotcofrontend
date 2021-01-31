e63d2fda6292000d0af23945f7b25aa6ec2129ee
Adrian Kokai <adriankokai@gmail.com>
2021-01-31 14:35:22 +0200

    updated host url for LinksInBio images

--- src/components/LinkInBio.js
+++ src/components/LinkInBio.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BACKEND_URL } from '../store/utility';
 
 export class LinkInBio extends Component {
     render() {
         return (
             <React.Fragment>
                 <a href={this.props.link} className="col s12 m6 l4">
-                    <img style={styles} src={'http://127.0.0.1:8000/main' + this.props.image} alt={this.props.title} />
+                    <img style={styles} src={BACKEND_URL + 'main' + this.props.image} alt={this.props.title} />
                 </a>
             </React.Fragment>
         )
