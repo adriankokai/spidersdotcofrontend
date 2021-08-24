3c9c51a192ee90c6ac928eacad42765d7d393b30
Adrian Kokai <adriankokai@gmail.com>
2021-08-24 07:45:28 +0200

    add pages to Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Blog from '../src/pages/Blog'
 import Portfolio from '../src/pages/Portfolio'
 import "materialize-css/dist/css/materialize.min.css";
+import BlogPost from './pages/BlogPost';
 
 export class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export class App extends Component {
       <Router>
           <Route exact path='/' component={Blog} />
           <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/blogpost/:id' component={BlogPost} />
       </Router>
     )
   }
