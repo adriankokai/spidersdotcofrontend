1d9d9b9ac2f7c7bd401dd08d872fe93549318f83
Adrian Kokai <adriankokai@gmail.com>
2026-01-31 12:37:24 +0200

    switch Navcard code from class component to function component

--- src/components/navcard/Navcard.js
+++ src/components/navcard/Navcard.js
@@ -2,6 +2,81 @@ import React, { Component } from 'react'
 import white from '../../images/white.jpg'
 import white1 from '../../images/white1.jpg'
 
+
+// Navcard component for navigation header
+
+//switching to functional component --- IGNORE ---
+const Navcard = (props) => {
+    const bg = props.portfolio ? white1:white
+
+    const cardStyles = {
+        backgroundImage:`url(${bg})`,
+        backgroundRepeat: "noRepeat",
+        height: '90vh',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        paddingTop: '2.5rem',
+        backdropFilter: 'blur(50px)',
+    }
+
+    const linkButtonStyles ={
+    marginLeft: '10px',
+    fontSize: '0.7rem'
+    }
+
+    const linkButtonStylesActive ={
+        marginLeft: '10px',
+        fontSize: '0.7rem',
+        opacity: '50%'
+    }
+
+    return (
+        <div class="row" style={{margin: '1rem'}} >
+            <div class="col s12" style={cardStyles} >
+                <div class="card transparent z-depth-0" >
+                        <div className="hide-on-med-and-down">
+                            <a 
+                                href="#" 
+                                className="btn transparent z-depth-0 black-text " 
+                                style={{fontSize: '1.8rem', fontFamily: 'freight-big-pro'}}
+                            >
+                                spidersdotco
+                            </a> 
+                            <a 
+                                href="/portfolio" 
+                                className="btn black right" 
+                                style={props.portfolio ? linkButtonStylesActive : linkButtonStyles}
+                            >
+                                portfolio
+                            </a>
+                            <a 
+                                href="/"
+                                className="btn black right" 
+                                style={props.blog ? linkButtonStylesActive : linkButtonStyles}
+                            >
+                                blog
+                            </a>
+                        </div>
+                        <h3 className="center " style={{marginTop: '30vh', fontFamily: 'freight-big-pro'}} >
+                            {
+                                props.portfolio ?
+
+                                "the web that spiders build"
+
+                                :
+
+                                "spiders that do what spiders do"
+                            }
+                        </h3>
+                        <hr className=" black" style={{width: '40px'}} />
+                </div>
+            </div>
+        </div>
+    )       
+}
+
+// classs component
+{/*}
 export class Navcard extends Component {
     bg = this.props.portfolio ? white1:white
     render() {
@@ -75,5 +150,6 @@ const linkButtonStylesActive ={
     fontSize: '0.7rem',
     opacity: '50%'
 }
+*/}
 
 export default Navcard
