5ac1d007cdd1129299fc0de7192f9e22eca65408
Adrian Kokai <adriankokai@gmail.com>
2026-05-20 15:47:58 +0200

    dropdown for creating new inventory, bills, receipts etc

--- src/components/accounts/navbar/CreateDropdown.js
+++ src/components/accounts/navbar/CreateDropdown.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-export default function CreateDropdown() {
+export default function CreateDropdown(props) {
     const createDropdownRef = React.useRef(null);
 
      React.useEffect(() => {
@@ -17,11 +17,11 @@ export default function CreateDropdown() {
         <i class="material-icons white-text text-darken-2 ">add</i>
     </a>
   <ul id="create-dropdown" className="dropdown-content">
-    <li><a href="#!" className='teal-text' ><i class="material-icons teal-text">receipt</i>Create Receipt</a></li>
-    <li><a href="#!" className='teal-text' ><i class="material-icons teal-text">description</i>Create Invoice</a></li>
-    <li><a href="#!" className='teal-text' ><i class="material-icons teal-text">receipt</i>Create Bill</a></li>
-    <li><a href="#!" className='teal-text' ><i class="material-icons teal-text">inventory</i>Create Inventory Item</a></li>
-    <li><a href="#!" className='teal-text' ><i class="material-icons teal-text">account_balance</i>Create Chart of Account</a></li>
+    <li onClick={() => props.changeMainAreaContent("displayCreateReceipt") } ><a href="#!" className='teal-text' ><i class="material-icons teal-text">receipt</i>Create Receipt</a></li>
+    <li onClick={() => props.changeMainAreaContent("displayCreateInvoice") } ><a href="#!" className='teal-text' ><i class="material-icons teal-text">description</i>Create Invoice</a></li>
+    <li onClick={() => props.changeMainAreaContent("displayCreateBill") } ><a href="#!" className='teal-text' ><i class="material-icons teal-text">receipt</i>Create Bill</a></li>
+    <li onClick={() => props.changeMainAreaContent("displayCreateProductService") } ><a href="#!" className='teal-text' ><i class="material-icons teal-text">inventory</i>Create Inventory Item</a></li>
+    <li onClick={() => props.changeMainAreaContent("displayCreateChartOfAccounts") } ><a href="#!" className='teal-text' ><i class="material-icons teal-text">account_balance</i>Create Chart of Account</a></li>
   </ul>
   </>
   )
